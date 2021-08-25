0ef7c41b7175a8dcf28399f826271b1dafb757c7
Siyuan Peng <siyuan.kevin.peng@gmail.com>
2021-08-25 01:55:11 -0400

    add save to json function

--- save_to_json.py
+++ save_to_json.py
@@ -1,5 +1,5 @@
 from vkitti_to_kitti import *
-import tqdm
+import tqdm, json
 
 # These constant are imported from my projects:
 CROP_RESIZE_H, CROP_RESIZE_W = 224, 224
@@ -18,9 +18,9 @@ VIEW_ANGLE_TOTAL_Y = 0.55850536064
 def store_all_object_to_json(label_dir, image_dir):
     # store all objects
     all_objs = []
-
     image_dir = get_all_img_path(image_dir, True)
-    for path_to_img in tqdm.tqdm(image_dir):
+
+    for path_to_img in tqdm.tqdm(np.random.choice(image_dir, 5)):
         df_label = get_label(path_to_img, label_dir=label_dir)
         if isinstance(df_label, bool) and df_label == False:
             continue
@@ -42,12 +42,18 @@ def store_all_object_to_json(label_dir, image_dir):
             'loc_y': float(camera_space_Y),
             'loc_z': float(camera_space_Z),
             'rot-y': float(rotation_camera_space_y),
-            'line': None,  # construct big space sperated string, kitti style
+            'line': f'{type} {float(truncation_ratio)} {float(occupancy_ratio)} {float(alpha)} {int(left)}'
+                    f' {int(bottom)} {int(right)} {int(top)} {height} {width} {length} {camera_space_X} {camera_space_Y}'
+                    f'{camera_space_Z} {rotation_camera_space_y}',  # construct big space sperated string, kitti style
             'view_angle': ((float(left) + float(right)) / 2) / CROP_RESIZE_W * VIEW_ANGLE_TOTAL_X - (
                         VIEW_ANGLE_TOTAL_X / 2)
         } for type, truncation_ratio, occupancy_ratio, alpha, left, top, right, bottom, height, width, length,
               camera_space_X, camera_space_Y, camera_space_Z, rotation_camera_space_y in df_label.to_numpy()]
         all_objs.append(objs)
+
+        # dump everything to json
+        with open('data.json', 'w') as f:
+            json.dump(all_objs, f)
     return all_objs
 
 
